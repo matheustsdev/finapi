3e3505cd16c9d6c1cd94276af106b625abead76f
Matheus Teixeira da Silva <matheusts.dev@gmail.com>
2022-07-26 10:10:41 -0300

    feat: create account n get statement

---
+++ src/index.js
@@ -0,0 +1,60 @@
+
+const {v4: uuidv4} = require("uuid")
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const customers = []
+
+function verifyIfExistsAccountCPF(request, response, next) {
+    const {cpf} = request.headers
+    
+    const customer = customers.find(customer => {
+        return customer.cpf === cpf
+    })
+    
+    if(!customer) {
+        return response.status(400).json({error: "Costumer not found."})
+    
+    }
+    
+    request.customer = customer
+    
+    return next()
+}
+
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body;
+    
+    const customerAlreadyExists = customers.some((customer) => {
+        return customer.cpf === cpf
+    
+    })
+    
+    if(customerAlreadyExists) {
+        return response.status(400).json({
+            error: "Customer already exists!"
+        })
+    }
+    
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+        
+    })
+    
+   return response.status(201).send()
+
+})
+
+app.get("/statement",verifyIfExistsAccountCPF,(request, response) => {
+    const {customer} = request
+
+    return response.json(customer.statement)
+})
+
+app.listen(3333)
